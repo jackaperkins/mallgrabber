e1bd50e4ec964dc79f8b880bf5d1508e12d05d46
jack perkins <jackaperkins@posteo.net>
2021-05-01 22:46:53 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('mallgrabbers unite on planet earth1!! shoutout fantasia malware krew!!!!')
+
+let s = 'https://streetviewpixels-pa.googleapis.com/v1/tile?cb_client=maps_sv.tactile&panoid=Mq-8d-CoiFnoVK7f2T8jVg&x=2&y=4&zoom=4&nbt=1&fover=2'
+let panoid = 'Mq-8d-CoiFnoVK7f2T8jVg'
+let x = 0
+let y = 0
+let zoom = 4
